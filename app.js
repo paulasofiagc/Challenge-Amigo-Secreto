af8a37118d36f8ac6fbf30d769c84723c3d23520
Paula Gallardo <paulasofiagc@gmail.com>
2025-08-07 02:14:00 -0400

    feat:  implementa funcionalidad de sorteo de amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+
+function agregarAmigo() {
+  const input = document.getElementById('amigo')
+  const nombre = input.value.trim()
+
+  if (!nombre) {
+    alert('Por favor, inserta un nombre válido')
+    return
+  }
+
+  amigos.push(nombre)
+
+  const lista = document.getElementById('listaAmigos')
+  const nuevoLi = document.createElement('li')
+  nuevoLi.textContent = nombre
+  nuevoLi.classList.add('name-item')
+  lista.appendChild(nuevoLi)
+
+  input.value = ''
+}
+
+function actualizarListaAmigos() {
+  const lista = document.getElementById('listaAmigos')
+  lista.innerHTML = ''
+
+  for (let i = 0; i < amigos.length; i++) {
+    lista.innerHTML += `<li class="name-item">${amigos[i]}</li>`
+  }
+}
+
+function sortearAmigo() {
+  const resultado = document.getElementById('resultado')
+
+  if (amigos.length === 0) {
+    alert('No hay amigos para sortear. Agrega al menos uno')
+    return
+  }
+
+  const index = Math.floor(Math.random() * amigos.length)
+  const ganador = amigos[index]
+
+  resultado.innerHTML = `<li class="result-item">🎉 El amigo secreto es: <strong>${ganador}</strong> 🎁</li>`
+}
